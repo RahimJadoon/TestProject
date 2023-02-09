03cadb463ebf72cc4792b0306ad684da2ad8d884
Rahim khan <rahimk593@gmail.com>
2023-02-09 21:11:20 +0500

    Added new comment as part of newbranch

--- src/Inheritance/tasteTester.java
+++ src/Inheritance/tasteTester.java
@@ -7,6 +7,8 @@ public class tasteTester {
         System.out.println("Cake flavor: " + cake.getFlavor());
         System.out.println("Cake price: " + cake.getPrice());
 
+        // birthday Cake
+
         cake birthdaycake= new cake("test");
         birthdaycake.setPrice(200);
         System.out.print("Flavor of BirthDayCake is: "+ birthdaycake.getFlavor()+ "and it's price is: "+ birthdaycake.getPrice());
